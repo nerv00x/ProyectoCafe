be5ad983fdcff81cad1a7b884cd392706cc6da2d
aridane <aridanecabreraramirez@gmail.com>
2023-11-30 17:56:11 +0100

    Prueba

--- js/app.js
+++ js/app.js
@@ -5,3 +5,66 @@ $(window).scroll(function(){
       $('nav').removeClass('animate');
     }
   })
+
+  function desglosarNumero() {
+    const numero = parseInt(document.getElementById('numeroInput').value);
+    const resultadoElement = document.getElementById('resultado');
+
+    if (numero >= 3 && numero <= 8) {
+      const desgloses = generarDesgloses(numero);
+      resultadoElement.innerHTML = ''; // Limpiar el desplegable antes de añadir nuevos elementos
+
+      if (desgloses.length > 0) {
+        desgloses.forEach(desglose => {
+          const option = document.createElement('option'); // Crear un nuevo elemento option
+          option.text = desglose;
+          resultadoElement.add(option);
+        });
+      } else {
+        const option = document.createElement('option');
+        option.text = 'No hay combinaciones posibles para este número.';
+        resultadoElement.add(option);
+      }
+    } else {
+      const option = document.createElement('option');
+      option.text = 'Por favor, ingrese un número entre 3 y 8.';
+      resultadoElement.add(option);
+    }
+  }
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const numeroInput = document.getElementById('numeroInput');
+    numeroInput.addEventListener('input', desglosarNumero);
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
